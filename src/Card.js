77cc4f36405d2b3ad4c7becbee487ad117100014
Christian Di Costanzo <80069360+ChristianDCUala@users.noreply.github.com>
2022-02-27 23:04:22 -0300

    Functional components

--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 
-class Card extends Component {
-
-    render() {
-        return (
-            <div>
-                <div className="card">
-                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: "100%" }} />
-                    <div className="container">
-                        <h4><b>{props.name}</b></h4>
-                        <p>{props.title}</p>
-                        <input type="text" onChange={props.onChangeName}></input>
-                        <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
-                        <div>{props.children}</div>
-                    </div>
+const Card = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <div className="card">
+                <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: "100%" }} />
+                <div className="container">
+                    <h4><b>{props.name}</b></h4>
+                    <p>{props.title}</p>
+                    <input type="text" onChange={props.onChangeName}></input>
+                    <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
+                    <div>{props.children}</div>
                 </div>
             </div>
-        )
-
-    }
+        </div>
+    )
 }
 
 export default Card;
