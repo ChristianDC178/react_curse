990ab1bf961000dcd33b55c27d3058b00dfe75da
Christian Di Costanzo <80069360+ChristianDCUala@users.noreply.github.com>
2022-03-01 20:56:46 -0300

    redux example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { BrowserRouter } from "react-router-dom";
 
 //Apps
 //import App from './App';
@@ -8,7 +10,41 @@ import './index.css';
 //import ReactContextApp from './ReactContextApp';
 //import UseReducerApp from './UseReducerApp'
 import RouterApp from './apps/RouterApp'
-import { BrowserRouter } from "react-router-dom";
+
+//actions
+const increment = (value) => {
+  return { type: 'INCREMENT', payload: value }
+}
+
+const decrement = (value) => {
+  return { type: 'DECREMENT', payload: value }
+}
+//reducers
+const counterReducer = (state = 0, action) => {
+
+  if (!action.type) return state
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + action.payload
+    case 'DECREMENT':
+      return state - action.payload
+    default:
+      return state
+  }
+}
+
+//store
+let store = createStore(counterReducer)
+//display
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+//dispatch the action
+store.dispatch(increment(5))
+store.dispatch(decrement(4))
+
 
 ReactDOM.render(
   <React.StrictMode>
